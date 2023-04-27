eb50bb6b83aafe51629ffa4e7c7a06c345cfc70c
Simon Ledoux <simon@simon511000.fr>
2023-04-27 12:37:35 +0200

    delete unnecessary comments

--- home.nix
+++ home.nix
@@ -15,34 +15,12 @@
 
   home.file = {
     ".config/autostart/autostart-guake.desktop".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix-profile/share/guake/autostart-guake.desktop";
-
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/simon/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     EDITOR = "nano";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   programs.git = {
@@ -50,16 +28,6 @@
     userName = "Simon Ledoux";
     userEmail = "simon@simon511000.fr";
   };
-
-  # programs.fish = {
-  #   enable = true;
-  #   interactiveShellInit = ''
-  #     set fish_greeting # Disable greeting
-  #   '';
-  #   shellAliases = {
-  #     cat = "bat";
-  #   };
-  # };
 
   programs.zsh = {
     enable = true;
