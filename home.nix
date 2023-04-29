7466d6137e964e2020089834a5d3e8476899c616
Simon Ledoux <simon@simon511000.fr>
2023-04-29 09:57:32 +0200

    Add catppuccin to bat

--- home.nix
+++ home.nix
@@ -7,6 +7,12 @@ let
     tweaks = [ "normal" ];
     variant = "macchiato";
   };
+  catppuccin-bat = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "bat";
+    rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+    sha256 = "6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+  };
 in
 {
   targets.genericLinux.enable = true;
@@ -92,6 +98,8 @@ in
 
   programs.bat = {
     enable = true;
+    themes.Catppuccin-macchiato = builtins.readFile "${catppuccin-bat}/Catppuccin-macchiato.tmTheme";
+    config.theme = "Catppuccin-macchiato";
   };
 
   gtk = {
