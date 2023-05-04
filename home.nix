d004d8c8f72fbb8a84cfc5f7052d7504c92b0eac
Simon Ledoux <simon@simon511000.fr>
2023-05-04 08:57:37 +0200

    Add support for various key in zsh #2

--- home.nix
+++ home.nix
@@ -76,6 +76,10 @@ in
       bindkey  "^[[H"   beginning-of-line
       bindkey  "^[[F"   end-of-line
       bindkey  "^[[3~"  delete-char
+      bindkey "^[[1;5C" forward-word
+      bindkey "^[[1;5D" backward-word
+      bindkey "^[[3;5~" kill-word
+      bindkey   "^H"    backward-kill-word
     '';
     
     enableAutosuggestions = true;
